99b924113a5725e12c989f3978317f84bff5abb9
limoe <zy143l@dmkuai.cn>
2022-01-14 09:06:31 +0800

    update

--- wskey.py
+++ wskey.py
@@ -368,10 +368,10 @@ def get_version():
     url = 'http://127.0.0.1:{0}/api/system'.format(port)
     try:
         res = s.get(url)
+        version = str(json.loads(res.text)['data']['version'])
     except:
         return 0
     else:
-        version = str(json.loads(res.text)['data']['version'])
         logger.info("青龙面板版本: " + version)
         if version > '2.10.13':
             return 1
